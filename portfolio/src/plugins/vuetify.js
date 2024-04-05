e0f8086b4b0b14ce39616fe1e73c9fbe16d640d8
dhanush <dhanush183001021@eee.ssn.edu.in>
2024-04-05 17:07:53 +0400

    added vue and vuetify using cli

---
+++ portfolio/src/plugins/vuetify.js
@@ -0,0 +1,10 @@
+// Styles
+import '@mdi/font/css/materialdesignicons.css'
+import 'vuetify/styles'
+import '@fontsource/open-sans'
+// Vuetify
+import { createVuetify } from 'vuetify'
+
+export default createVuetify(
+  // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
+)
